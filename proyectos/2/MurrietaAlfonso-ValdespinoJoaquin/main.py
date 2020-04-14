c97664cd5317960c841d0fb66613ea848712990d
aMurryFly <alfonsomvmx@outlook.com>
2020-04-14 17:53:46 -0500

    Correción de main, test 2 - Checar concurrencia

--- proyectos/2/MurrietaAlfonso-ValdespinoJoaquin/main.py
+++ proyectos/2/MurrietaAlfonso-ValdespinoJoaquin/main.py
@@ -1,42 +1,48 @@
-from threading import RLock, Thread 
+from threading import RLock, Thread #RLock usado en caso de usar .acquiare por segunda vez desde el mismo hilo
 from time import sleep
 
+# Modulos propios ->Carpeta modulo
 from modulos.agencia import agencia
 from modulos.compania import compania
 from modulos.cliente import cliente
 
 
-lock = RLock()
-lock.acquire()
-lock.release()
+if __name__ == "__main__":
 
-hilosList = []
+    lock = RLock()
+    lock.acquire()
+    lock.release()
 
-clienteList = []
-companiaList = []
-agenciaList = []
+    hilosList = []
 
+    clienteList = []
+    companiaList = []
+    agenciaList = []
 
-aereomar = compania('Aeromar')
-aereomexico = compania('Aereomexico')
-interjet = compania('InterJet')
-volaris = compania('volaris')
+    #compañias de vuelo
 
-companiaList = [aereomar,aereomexico,interjet,volaris]
+    aereomar = compania('Aeromar')
+    aereomexico = compania('Aereomexico')
+    interjet = compania('InterJet')
+    volaris = compania('volaris')
 
-coppel = agencia(companiaList,'coppelViajes')
-despegar = agencia(companiaList, 'despegar.com')
-mundo  = agencia(companiaList, "mundomex")
-best = agencia(companiaList, 'bestday')
-palacio = agencia(companiaList, 'viajesPalacio')
+    companiaList = [aereomar,aereomexico,interjet,volaris]
 
-agenciaList = [coppel,despegar,mundo,best,palacio]
+    #agencias de vuelo
+    coppel = agencia(companiaList,'coppelViajes')
+    despegar = agencia(companiaList, 'despegar.com')
+    mundo  = agencia(companiaList, "mundomex")
+    best = agencia(companiaList, 'bestday')
+    palacio = agencia(companiaList, 'viajesPalacio')
 
-for agencias in agenciaList:
-    agencias.run()
+    agenciaList = [coppel,despegar,mundo,best,palacio]
 
-sleep(2)
+    for agencias in agenciaList:
+        agencias.run()
 
-for i in range(10):
-    clienteList.append( cliente(agenciaList))
-    hilosList.append(Thread(target=clienteList[i].run).start())
+    sleep(2)
+
+    # creando clientes y corriendo los hilos correspondientes
+    for i in range(5):
+        clienteList.append( cliente(agenciaList))
+        hilosList.append(Thread(target=clienteList[i].run).start())
