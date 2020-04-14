178d7074170545e6b3f9b35187d4f9420e7f2372
loidijavier1 <javier.loidi.gtz@gmail.com>
2020-04-13 20:03:36 -0500

    Agrego el avance 4 del proyecto 2, donde ya se incluyeron todas las funciones pero todavia hay errores a corregir

--- proyectos/2/LoidiJavier/ensamblaje_de_autos.py
+++ proyectos/2/LoidiJavier/ensamblaje_de_autos.py
@@ -39,7 +39,13 @@ hay_motor = Semaphore(0)
 hay_seccion_de_chasis = Semaphore(0)
 hay_seccion_de_transmision = Semaphore(0)
 hay_llantas = Semaphore(0)
-hay_cristales = Semaphore(0)
+hay_pintura = Semaphore(0)
+
+hay_motor_chasis = Semaphore(0)
+hay_transmision_llantas = Semaphore(0)
+hay_auto_sin_pintura = Semaphore(0)
+
+mutex_completado = Semaphore(0)
 
 def constructor_motor():
     global mutex_motor, cuenta_motor, total_motor, se_puede_seccion_de_chasis
@@ -80,7 +86,7 @@ def constructor_seccion_de_transmision():
     global mutex_seccion_de_transmision, cuenta_seccion_de_transmision, total_seccion_de_transmision
 
     mutex_print.acquire()
-    print("\n Se construyó la seccion de transmision numero",total_seccion_de_transmision)
+    print("\n Se construyó la seccion de transmision numero",total_seccion_de_transmision+1)
     mutex_print.release()
 
     mutex_seccion_de_transmision.acquire()
@@ -88,7 +94,7 @@ def constructor_seccion_de_transmision():
     cuenta_seccion_de_transmision = cuenta_seccion_de_transmision + 1
     total_seccion_de_transmision = total_seccion_de_transmision + 1
 
-    if(cuenta_seccion_de_transmision == 3):
+    if(cuenta_seccion_de_transmision > 3):
         hay_seccion_de_transmision.release()
 
     mutex_seccion_de_transmision.release()
@@ -106,16 +112,31 @@ def constructor_llantas():
     print("\n Se construyó la llanta numero",total_llantas)
     mutex_print.release()
 
-    if(cuenta_llantas == 4):
+    if(cuenta_llantas > 3):
         hay_llantas.release()
 
     mutex_llantas.release()
 
 
-def ensamblar_motor_con_seccion_de_chasis():
-    global hay_motor, hay_seccion_de_chasis, cuenta_motor, cuenta_seccion_de_chasis, total_motor_chasis, mutex_motor_chasis
+def mezclador_pintura():
+    global mutex_pinturas, cuenta_pinturas, total_pinturas
 
-    
+    mutex_pinturas.acquire()
+
+    cuenta_pinturas = cuenta_pinturas + 1
+    total_pinturas = total_pinturas + 1
+
+    mutex_print.acquire()
+    print("\n Se mezclo la pintura numero",total_pinturas)
+    mutex_print.release()
+
+    mutex_pinturas.release()
+
+    hay_pintura.release()
+
+
+def ensamblar_motor_con_seccion_de_chasis():
+    global hay_motor, hay_seccion_de_chasis, cuenta_motor, cuenta_seccion_de_chasis, total_motor_chasis, mutex_motor_chasis, mutex_motor, mutex_seccion_de_chasis, cuenta_motor_chasis
 
     hay_motor.acquire()
     hay_seccion_de_chasis.acquire()
@@ -124,69 +145,144 @@ def ensamblar_motor_con_seccion_de_chasis():
 
     total_motor_chasis = total_motor_chasis + 1
 
-    mutex_print.acquire()
-    print("\n --> Hay",cuenta_motor,"motor/es y",cuenta_seccion_de_chasis,"secciones de chasis.")
-    print("\n --> Se ensamblo 1 motor con 2 secciones de chasis. Se ensamblo el motor-chasis numero",total_motor_chasis)
-    mutex_print.release()
-
     mutex_motor.acquire()
     mutex_seccion_de_chasis.acquire()
+
+    mutex_print.acquire()
+    print("\n ---> Hay",cuenta_motor,"motor/es y",cuenta_seccion_de_chasis,"secciones de chasis.")
+    print("\n ---> Se ensamblo 1 motor con 2 secciones de chasis. Se ensamblo el motor-chasis numero",total_motor_chasis)
+    mutex_print.release()
 
     cuenta_motor = cuenta_motor - 1
     cuenta_seccion_de_chasis = cuenta_seccion_de_chasis - 2
 
+    cuenta_motor_chasis = cuenta_motor_chasis + 1
+
     mutex_seccion_de_chasis.release()
     mutex_motor.release()
+
+    hay_motor_chasis.release()
 
     mutex_motor_chasis.release()
 
 
-
-    
-
-
-
 def ensamblar_transmision_con_llantas():
-    global hay_seccion_de_transmision, hay_llantas, cuenta_seccion_de_transmision, cuenta_llantas, total_transmision_llantas, mutex_transmision_llantas
+    global hay_seccion_de_transmision, hay_llantas, cuenta_seccion_de_transmision, cuenta_llantas, total_transmision_llantas, mutex_transmision_llantas, mutex_seccion_de_transmision, mutex_llantas, cuenta_transmision_llantas
 
     hay_seccion_de_transmision.acquire()
     hay_llantas.acquire()
-
-    mutex_seccion_de_transmision.acquire()
-    mutex_llantas.acquire()
 
     mutex_transmision_llantas.acquire()
 
     total_transmision_llantas = total_transmision_llantas + 1
 
+    mutex_seccion_de_transmision.acquire()
+    mutex_llantas.acquire()
+
     mutex_print.acquire()
-    print("\n --> Hay",cuenta_seccion_de_transmision,"secciones de transmision y",cuenta_llantas,"llantas.")
-    print("\n --> Se ensamblo 3 secciones de transmision con 4 llantas. Se ensamblo el transmision-llantas numero",total_transmision_llantas)
+    print("\n ---> Hay",cuenta_seccion_de_transmision,"secciones de transmision y",cuenta_llantas,"llantas.")
+    print("\n ---> Se ensamblo 4 secciones de transmision con 4 llantas. Se ensamblo el transmision-llantas numero",total_transmision_llantas)
     mutex_print.release()
 
-    cuenta_seccion_de_transmision = cuenta_seccion_de_transmision - 3
+    cuenta_seccion_de_transmision = cuenta_seccion_de_transmision - 4
     cuenta_llantas = cuenta_llantas - 4
 
-    mutex_transmision_llantas.release()
+    cuenta_transmision_llantas = cuenta_transmision_llantas + 1
 
     mutex_llantas.release()
     mutex_seccion_de_transmision.release()
+
+    hay_transmision_llantas.release()
+
+    mutex_transmision_llantas.release()
+
+
+def ensamblar_auto():
+    global hay_motor_chasis, hay_transmision_llantas, cuenta_autos_sin_pintura, total_autos_sin_pintura, cuenta_motor_chasis, cuenta_transmision_llantas, mutex_autos_sin_pintura, mutex_motor_chasis, mutex_transmision_llantas
+
+    hay_motor_chasis.acquire()
+    hay_transmision_llantas.acquire()
+
+    mutex_autos_sin_pintura.acquire()
+
+    total_autos_sin_pintura = total_autos_sin_pintura + 1
+
+    mutex_motor_chasis.acquire()
+    mutex_transmision_llantas.acquire()
+
+    mutex_print.acquire()
+    print("\n ------> Hay",cuenta_motor_chasis,"motor-chasis y",cuenta_transmision_llantas,"transmision-llantas.")
+    print("\n ------> Se ensamblo 1 motor-chasis con 1 transmision-llantas. Se ensamblo el auto sin pintura numero",total_autos_sin_pintura)
+    mutex_print.release()
+
+    cuenta_motor_chasis = cuenta_motor_chasis - 1
+    cuenta_transmision_llantas = cuenta_transmision_llantas - 1
+
+    mutex_transmision_llantas.release()
+    mutex_motor_chasis.release()
+
+    hay_auto_sin_pintura.release()
+
+    mutex_autos_sin_pintura.release()
+
+
+def pintar_auto():
+    global hay_auto_sin_pintura, hay_pintura, cuenta_pinturas, total_autos, mutex_autos, mutex_completado
+
+    hay_auto_sin_pintura.acquire()
+    hay_pintura.acquire()
+
+    mutex_autos.acquire()
+
+
+    total_autos = total_autos + 1
+
+    mutex_print.acquire()
+    print("\n >>>>>>>>>> Se pinto el auto numero",total_autos,"y esta listo para su venta.")
+    mutex_print.release()
+
+    if(total_autos > 11):
+        mutex_completado.release()
+
+    mutex_autos.release()
     
 
 
 
 
-for i in range(20):
-    if (i%2==0):
+for i in range(48):
+    if (i%4==0):
         t1 = Thread(target=constructor_motor,args=[])
         t1.start()
 
-        t3 = Thread(target=ensamblar_motor_con_seccion_de_chasis,args=[])
+        t2 = Thread(target=ensamblar_motor_con_seccion_de_chasis,args=[])
+        t2.start()
+
+        t3 = Thread(target=ensamblar_transmision_con_llantas,args=[])
         t3.start()
 
+        t_pintura = Thread(target=mezclador_pintura,args=[])
+        t_pintura.start()
 
-    t2 = Thread(target=constructor_seccion_de_chasis,args=[])
-    t2.start()
+        te_sp = Thread(target=ensamblar_auto,args=[])
+        te_sp.start()
+
+        te_p = Thread(target=pintar_auto,args=[])
+        te_p.start()
+
+    if (i%2==0):
+        t4 = Thread(target=constructor_seccion_de_chasis,args=[])
+        t4.start()
+
+    t5 = Thread(target=constructor_seccion_de_transmision,args=[])
+    t5.start()
+
+    t6 = Thread(target=constructor_llantas,args=[])
+    t6.start()
+
+
+mutex_completado.acquire()
+print("\n\n  ---  TODOS LOS COCHES ESTAN COMPLETADOS ---")
 
 
 
