5e7325f99f5885e726d9903143f4dd41c489bd8a
fernando170 <fer-170@live.com.mx>
2020-05-20 17:20:49 -0500

    Reparar el mount

--- proyectos/4/Medina Fernando, Osorio Sergio/FiUnamFs.py
+++ proyectos/4/Medina Fernando, Osorio Sergio/FiUnamFs.py
@@ -1,4 +1,13 @@
-def main():
-D
-D
-	f=open("")
+import os
+carp='/fiunamfs'
+print("********************************************************\n**********************ADVERTENCIA***********************\n********************************************************\nLos comandos que serán utilizados requieren de que se \nejecuten como sudo o su, por ello se necesitará la \nejecución como sudo python3 FiUnamFs.py o brindar de \nmanera manual el permiso, cada que se le solicite")
+os.system('sudo mkfs -t ext4 ./fiunamfs.img')
+print('Creación de carpeta')
+os.system('sudo mkdir /mnt'+carp)
+os.system('sudo mount -r -t auto -o loop ./fiunamfs.img /mnt'+carp)
+#print('Disco montado en /mnt/fiunamfs')
+os.system('df -hT')
+os.system('sudo umount /mnt'+carp)
+print('Eliminación de la carpeta')
+os.system('sudo rmdir /mnt'+carp)
+
