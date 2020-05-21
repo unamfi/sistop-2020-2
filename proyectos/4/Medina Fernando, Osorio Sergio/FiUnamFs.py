ef7718a786396a44bdfe269830f7053dc3357d70
fernando170 <fer-170@live.com.mx>
2020-05-20 21:12:23 -0500

    Comenzamos con la interfaz gráfica

--- proyectos/4/Medina Fernando, Osorio Sergio/FiUnamFs.py
+++ proyectos/4/Medina Fernando, Osorio Sergio/FiUnamFs.py
@@ -1,27 +1,72 @@
 import os
 import time
-import tkinter
+import tkinter as tk 
 carp='/fiunamfs'
-print("********************************************************\n**********************ADVERTENCIA***********************\n********************************************************\nLos comandos que seran utilizados requieren de que se \nejecuten como sudo o su, por ello se necesitara la \nejecucion como sudo python3 FiUnamFs.py o brindar de \nmanera manual el permiso, cada que se le solicite")
-#le damos el formato al disco ext4
-os.system('sudo mkfs -t ext4 ./fiunamfs.img')
-#creamos la carpeta en la que se montara el disco
-os.system('sudo mkdir /mnt'+carp)
-print('Carpeta para montar creada')
-#Montado del disco en la carpeta creada
-os.system('sudo mount -r -t auto -o loop ./fiunamfs.img /mnt'+carp)
-print('Disco montado en /mnt'+carp)
-#Comprobacion del montado del disco
-#os.system('df -hT')
+
+def inicio():
+    print("********************************************************\n**********************ADVERTENCIA***********************\n********************************************************\nLos comandos que seran utilizados requieren de que se \nejecuten como sudo o su, por ello se necesitara la \nejecucion como sudo python3 FiUnamFs.py o brindar de \nmanera manual el permiso, cada que se le solicite")
+    #le damos el formato al disco ext4
+    os.system('sudo mkfs -t ext4 ./fiunamfs.img')
+    #creamos la carpeta en la que se montara el disco
+    os.system('sudo mkdir /mnt'+carp)
+    print('Carpeta para montar creada')
+    #Montado del disco en la carpeta creada
+    os.system('sudo mount -r -t auto -o loop ./fiunamfs.img /mnt'+carp)
+    print('Disco montado en /mnt'+carp)
+    #Comprobacion del montado del disco
+    os.system('df -hT')
+    
+"""FUNCIONALIDADES """
+def n():
+    gui()
+
+def o():
+    pass
+
+def rm():
+    pass
+
 '''Crearemos la interfaz grafica con tkinter'''
-vent_princip=tkinter.Tk()
-#name = tkinter.tkFileDialog.askopenfilename(initialdir = curr_directory,title = "Select file",filetypes = (("jpeg files","*.jpg"),("all files","*.*")))
-#print(name)
-vent_princip.mainloop()
-'''¡¡¡A LEVANTAR LOS JUGUETES!!!'''
-print('Desmontando el disco')
-os.system('sudo umount /mnt'+carp)
-print('Eliminacion de la carpeta')
-os.system('sudo rmdir /mnt'+carp)
-print('Carpeta eliminada')
+def gui():
+    os.system('cd /mnt/fiunamfs')
+    vent_princip=tk.Tk()
+    vent_princip.geometry('500x500')
+    vent_princip.title('Sistema de Archivos Fi-UNAM')    
+    menu = tk.Menu(vent_princip)
+    vent_princip.config(menu=menu)
+    filemenu = tk.Menu(menu) 
+    menu.add_cascade(label='Archivo', menu=filemenu) 
+    filemenu.add_command(label='Nuevo',command=n) 
+    filemenu.add_command(label='Abrir',command=o)
+    filemenu.add_command(label='Eliminar',command=rm)
+    filemenu.add_separator() 
+    filemenu.add_command(label='Salir', command=vent_princip.quit) 
+    helpmenu = tk.Menu() 
+    menu.add_cascade(label='Ayuda', menu=helpmenu) 
+    helpmenu.add_command(label='Acerca de ') 
+    #  
+    label1=tk.Label(vent_princip,text='Listado de archivos')
+    label1.grid(column=0,row=1)
+    #
+    lista = os.listdir('/mnt'+carp)
+    arch=tk.Listbox(vent_princip)
+    #arch.pack()
+    for i in lista:
+        arch.insert(tk.END, i)
+    arch.grid(column=0,row=2) 
+    vent_princip.mainloop()
+
+def fin():
+    
+    '''¡¡¡A LEVANTAR LOS JUGUETES!!!'''
+    print('Desmontando el disco')
+    os.system('sudo umount /mnt'+carp)
+    print('Eliminacion de la carpeta')
+    os.system('sudo rmdir /mnt'+carp)
+    print('Carpeta eliminada')
+
+if __name__=="__main__":
+    inicio()
+    gui()
+    fin()
 
