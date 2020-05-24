2d59c81dd4bb05f60deae15347a847cbb13ea4ff
JOBA25 <jorge_octavio25@outlook.com>
2020-05-23 22:50:34 -0400

    se completaron las funciones requeridas, falta implementar el modulo de Fuse

--- proyectos/4/BarcenasJorge_RezaSergio/proy_4.py
+++ proyectos/4/BarcenasJorge_RezaSergio/proy_4.py
@@ -1,19 +1,32 @@
+"""
+	Proyecto 4: (micro) sistema de archivos
+	Profesor: Gunnar Wolf
+
+	Alumnos:
+		Barcenas Avelar, Jorge Octavio
+		Reza Chavarria, Sergio Gabriel
+
+"""
 import mmap
 import os
 import sys
 import os.path, time
 import math
-# La superficie del disco se divide en sectores de 256 bytes.
-# Cada cluster mide cuatro sectores.
-#
-#
+"""
+ La superficie del disco se divide en sectores de 256 bytes.
+ Cada cluster mide cuatro sectores.
 
 
-
+	Comandos del programa
+    para mostrar el directorio usaremos         ls
+    para eliminar un archivo usaremos           rm [FILE]
+    para copiar un archivo a nuestro sistema    cpout [FILE]
+    para copiar un archivo al sistema           cpin [FILE]
+    para desfragmentar                          defrag
+"""
 class BloquePrincipal:
     #El primer cluster (#0) del pseudodispositivo es el superbloque. 
     # Este contiene información en los siguientes bytes
-    # chech de aquí saque el uso de mmap.mmap ;v https://rico-schmidt.name/pymotw-3/mmap/
     f = open('fiunamfs.img','r+b')
     fs_map = mmap.mmap(f.fileno(),0,access=mmap.ACCESS_READ)
 
@@ -30,12 +43,7 @@ class BloquePrincipal:
     fs_map.close()
 
 
-    #para mostrar el directorio usaremos         ls
-    #para eliminar un archivo usaremos           rm [FILE]
-    #para copiar un archivo a nuestro sistema    cpout [FILE]
-    #para copiar un archivo al sistema           cpin [FILE]
-    #para desfragmentar                          defrag
-
+#Clase para la obtención de información de las secciones del sistema que se requieren
 
 class Entrada:
     nombre_f    = 15
@@ -58,6 +66,7 @@ class Entrada:
         self.creacionF = entrada[31:45].decode('utf-8')
         self.modifF    = entrada[46:60].decode('utf-8')
 
+#Clase principal que utilizará el micro sistema, junto con las operaciones de este.
 class SistArch:
     f = open('fiunamfs.img','a+b')
     fs_map = mmap.mmap(f.fileno(),0,access=mmap.ACCESS_WRITE)
@@ -75,14 +84,14 @@ class SistArch:
             # con entradas de 64 
             #64 * 64 = 4096
             #64 * 63 = 4032 (63 porque sera el ultimo lugar donde podra entra una entrada)
-            # 1024 + ( [0,63] * 64 )  
+            #       1024                + ( [0,63] * 64 )  
             p = self.bp.tamanio_cluster + i * self.bp.tamanio_entrada
             j = Entrada(self.fs_map[p:p + self.bp.tamanio_entrada])
             if self.entrada_no_usada != j.nombreF:
                 j.numdir = i
                 inodos.append(j)
         return inodos
-
+    #Método de busqueda
     def buscar (self,archivo):
         for x in range(0,64):
             p = self.bp.tamanio_cluster + x * self.bp.tamanio_entrada
@@ -91,7 +100,7 @@ class SistArch:
                 j.numdir = x
                 return j
         return None
-    
+    #Método de registro de archivo
     def registrar(self,archivo,cluster):
         for x in range(0,64):
             p = self.bp.tamanio_cluster + x*self.bp.tamanio_entrada
@@ -126,13 +135,14 @@ class SistArch:
                 break
 
 
-
+    #Método de obtención de información
     def ls(self):
         print("{:15}{:15}{:10}{:6}{:6}{:6}{:6}".format("Nombre","Cluster","Tamaño","Mes","Dia","Año","Hora"))
         for i in self.inodo():
-            fecha = self.FormatoFecha(i.modifF)
-            print("{:15}{:8}{:12}{:24}".format(i.nombreF, i.clusterF, i.tamanioF, fecha))
-
+        	fecha_c= self.FormatoFecha(i.creacionF)
+        	fecha_m = self.FormatoFecha(i.modifF)
+        	print("{:15}{:8}{:12}{:24}{:24}".format(i.nombreF, i.clusterF, i.tamanioF, fecha_c,fecha_m))
+    #Método para la impresión de la fecha
     def FormatoFecha(self,fecha):
         meses={'01':'Ene','02':'Feb','03':'Mar','04':'Abr','05':'May',
             '06':'Jun','07':'Jul','08':'Ago','09':'Sept','10':'Oct','11':'Nov','12':'Dis'}
@@ -143,7 +153,7 @@ class SistArch:
         mm=fecha[10:12]
         ss=fecha[12:14]
         return "{:8}{:5}{:6}{:10}".format("\t"+m,d,a,hh+':'+mm+':'+ss)
-
+   	#Método para eliminación de información
     def rm(self,archivo):
         i = self.buscar(archivo)
         if i is None :
@@ -151,8 +161,13 @@ class SistArch:
         else :
             p = self.bp.tamanio_cluster + self.bp.tamanio_entrada *i.numdir
             self.fs_map[p:p + i.nombre_f] = bytes(self.entrada_no_usada,'utf-8')
-    
-    def cpout(self,archivo,dir):
+            self.fs_map[p+16:p+24]=bytes("".zfill(8),'utf-8')
+            self.fs_map[p+25:p+30]=bytes("".zfill(5),'utf-8')
+            self.fs_map[p+31:p+45]=bytes("".zfill(14),'utf-8')
+            self.fs_map[p+46:p+60]=bytes("".zfill(14),'utf-8')
+
+    #Método para copiar en nuestro sistema
+    def cpout(self,archivo):
         #Primero buscar si el archivo existe,
         #si existe, lo copiamos al directorio especificado
         i = self.buscar(archivo)
@@ -164,7 +179,7 @@ class SistArch:
             cluster = self.bp.tamanio_cluster * i.clusterF
             archivocp.write(self.fs_map[cluster:cluster + i.tamanioF])
             archivocp.close()
-
+    #Método para comprobar si es posible copiar al sistema FiUnamFS
     def cpin(self, archivo):
         if os.path.isfile(archivo):
             if len(archivo) < 15:
@@ -177,6 +192,7 @@ class SistArch:
         else:
             print("cpin: " + archivo + " Archivo no encontrado")
 
+    #Metodo de copiado de archivos hacia el sistema FiUnamFS
     def Copiar(self, archivo):
         inodos = self.inodo()
         inodos.sort(key = lambda x : x.clusterF)
@@ -226,14 +242,52 @@ class SistArch:
                     copiado = True
                 else:
                     print(archivo + "Archivo demasido grande")
+    #Método para desfragmentar 
+    def defrag(self):
+    	for i in range(0,64): 
+            p = self.bp.tamanio_cluster + i * self.bp.tamanio_entrada
+            j = Entrada(self.fs_map[p:p + self.bp.tamanio_entrada])
+            #print("{} + {} + {}".format(i,j.nombreF,j.clusterF))
+            if (j.nombreF==self.entrada_no_usada):
+            	for k in range (i+1,64):
+            		paux = self.bp.tamanio_cluster + k * self.bp.tamanio_entrada
+            		jaux = Entrada(self.fs_map[paux:paux + self.bp.tamanio_entrada])
+            		if(jaux.nombreF!=self.entrada_no_usada):
+            			self.fs_map[p:p + self.bp.tamanio_entrada]=self.fs_map[paux:paux + self.bp.tamanio_entrada]
+            			self.fs_map[paux:paux + 15]=bytes(self.entrada_no_usada,'utf-8')
+            			self.fs_map[paux+16:paux+24]=bytes("".zfill(8),'utf-8')
+            			self.fs_map[paux+25:paux+30]=bytes("".zfill(5),'utf-8')
+            			self.fs_map[paux+31:paux+45]=bytes("".zfill(14),'utf-8')
+            			self.fs_map[paux+46:paux+60]=bytes("".zfill(14),'utf-8')
+            			break
+    	print("Defrag Complete")
+    #Método de prueba para checar si se realizó la desfragmentación
+    def printall (self):
+    	for i in range (0,64):
+    		p = self.bp.tamanio_cluster + i * self.bp.tamanio_entrada
+    		j = Entrada(self.fs_map[p:p + self.bp.tamanio_entrada])
+    		print("{} + {} + {} + {} + {} + {}".format(i,j.nombreF,j.clusterF,j.creacionF,j.modifF,j.numdir))
 
-    #def defrag(self):
+def main():
+	sistema = SistArch()
+	sistema.printall()
+	sistema.defrag()
+	sistema.printall()
+	sistema.rm("logo.png")
+	sistema.defrag()
+	sistema.printall()
 
-            
+if __name__ == '__main__':
+    main()
 
 
 
-
-
+"""
+    def printall (self):
+    	for i in range (0,64):
+    		p = self.bp.tamanio_cluster + i * self.bp.tamanio_entrada
+    		j = Entrada(self.fs_map[p:p + self.bp.tamanio_entrada])
+    		print("{} + {} + {} + {} + {} + {}".format(i,j.nombreF,j.clusterF,j.creacionF,j.modifF,j.numdir))
+"""
 
 
