f7ff1beeb24689985993d9a758e70bd77c95877b
fernando170 <fer-170@live.com.mx>
2020-05-25 19:33:21 -0500

    Esta interfaz que no se deja

--- proyectos/4/Medina Fernando, Osorio Sergio/FiUnamFs.py
+++ proyectos/4/Medina Fernando, Osorio Sergio/FiUnamFs.py
@@ -1,4 +1,6 @@
 import os
+import sys
+import errno
 import time
 import tkinter as tk
 from tkinter import ttk 
@@ -8,42 +10,17 @@ carp='/fiunamfs'
 def inicio():
     print("********************************************************\n**********************ADVERTENCIA***********************\n********************************************************\nLos comandos que seran utilizados requieren de que se \nejecuten como sudo o su, por ello se necesitara la \nejecucion como sudo python3 FiUnamFs.py o brindar de \nmanera manual el permiso, cada que se le solicite")
     #le damos el formato al disco ext4
-    os.system('sudo mkfs -t ext4 ./fiunamfs.img')
+    os.system('sudo mkfs -t fat ./fiunamfs.img')
     #creamos la carpeta en la que se montara el disco
     os.system('sudo mkdir /mnt'+carp)
-    os.system('clear')
-    os.system('cowsay Carpeta para montar creada')
+    print('Carpeta para montar creada')
     #Montado del disco en la carpeta creada
-    os.system('sudo mount -r -t auto -o loop ./fiunamfs.img /mnt'+carp)
-    os.system('clear')
-    os.system('cowsay Disco montado en /mnt'+carp)
-    #Comprobacion del montado del disco
+    os.system('sudo mount ./fiunamfs.img /mnt'+carp)
+    print('Disco montado en /mnt'+carp)
+    #Comprobacion del montado del discopython
     os.system('df -hT')
     
 """FUNCIONALIDADES """
-def nuevoa(nombre,ext):
-	'''
-	Con la función nuevoa se pretende crear el archivo con la extensión deseada por el usuario, ya sea que el la ingrese manualmente o utilizando el combobox proporcionado con las opciones más comunes, el nombre estará dado por el parámetro nombre y la extensión por el parametro ext
-	:param nombre: Nombre del archivo que vamos a crear. type: String
-	:param ext: Nombre de la extensión que tendrá el archivo, puede ser nulo. type: String
-	'''
-	if(ext==''or ext==NULL):
-        	nuevo_arch=open(nombre,'w')
-        	tkinter.messagebox.showinfo(title='Archivo creado',message='El archvo '+nombre+' ha sido creado')
-	else:
-    		nuevo_arch=open(nombre+'.'+ext,'w')
-    		tkinter.messagebox.showinfo(title='Archivo creado',message='El archvo '+nombre+'.'+' ha sido creado')
-    	 
-def nuevac(nombre):
-	'''
-	Con ésta función crearemos en el disco una carpeta con el nombre específicado en el parámetro nom, en dado caso de que no se pueda generar la carpeta se informará al usuario a través de una ventana.
-	:param nombre: El nombre que se le dará a la nueva carpeta. type: String
-	'''
-	try:
-		os.mkdir('/mnt/'+carp+nombre)
-	except OSError:
-            	tk.messagebox.showinfo(title='Archivo creado',message='Fallo al intentar crear carpeta nueva, es posible que ya exista una con el mismo nombre.')
-
 def cierra():
 	
 	'''
@@ -56,11 +33,28 @@ def cierra():
 def nc():
     nuevo = tk.Toplevel()
     nuevo.title('Crear nueva carpeta')
-    ins=tk.Label(nuevo,text='Por favor ingrese el nombre de la carpeta a crear')
-    ins.grid(column=0,row=0)
-    nombre=tk.Entry(nuevo)
-    nombre.grid(column=0,row=1)
-    boton2=tk.Button(nuevo,text='Aceptar',command=nuevac(nombre.get()))
+    la=tk.Label(nuevo,text='Por favor ingrese el nombre de la carpeta a crear')
+    la.grid(column=0,row=0)
+    nombrec=tk.Entry(nuevo)
+    nombrec.grid(column=0,row=1)
+    def nuevac():
+        '''
+        Con ésta función crearemos en el disco una carpeta con el nombre específicado en el parámetro nom, en dado caso de que no se pueda generar la carpeta se informará al usuario a través de una ventana.
+        :param nombre: El nombre que se le dará a la nueva carpeta. type: String
+        '''
+        try:
+            os.mkdir('/mnt'+carp+str(nombrec.get()))
+            tk.messagebox.showinfo(title='Carpeta creada',message='La carpeta '+str(nombre.get())+' ha sido creada')
+        except OSError as e:
+            if e.errno == errno.EEXIST:
+                print('Directory not created.')
+            else:
+                raise
+                tk.messagebox.showinfo(title='Carpeta no creada',message='Fallo al intentar crear carpeta nueva, es posible que ya exista una con el mismo nombre.')
+                print(sys.exc_info()[0])
+                nuevo.destroy()
+    
+    boton2=tk.Button(nuevo,text='Aceptar',command=nuevac)
     boton2.grid(column=0,row=3)
     nuevo.mainloop()
     '''
@@ -70,18 +64,29 @@ def nc():
 
 def na():
     '''Con la función n crearemos un nuevo archivo con la '''
-    nuevo = tk.Toplevel()
-    nuevo.title('Crear nuevo archivo')
-    ins=tk.Label(nuevo,text='Por favor ingrese el nombre del archivo y seleccione la extensión deseada')
-    ins.grid(column=0,row=0)
-    nombre=tk.Entry(nuevo)
+    nueva = tk.Toplevel()
+    nueva.title('Crear nuevo archivo')
+    ins=tk.Label(nueva,text='Por favor ingrese el nombre del archivo y seleccione la extensión deseada').grid(column=0,row=0)
+    nombre=tk.Entry(nueva)
     nombre.grid(column=0,row=1)
-    ext=ttk.Combobox(nuevo, width=27,text='Elija uno')
+    ext=ttk.Combobox(nueva, width=27,text='Elija uno')
     ext['values']=('com','html','docx','pptx','jpg','png','exe','svg','ico')
     ext.grid(column=2,row=1)
-    boton=tk.Button(nuevo,text='Aceptar',command=nuevoa(nombre.get(),ext.current()))
-    boton.pack()
-    nuevo.mainloop()  	
+    print('creación de archivo')
+    def nuevoa():
+        '''
+        Con la función nuevoa se pretende crear el archivo con la extensión deseada por el usuario, ya sea que el la ingrese manualmente o utilizando el combobox proporcionado con las opciones más comunes, el nombre estará dado por el parámetro nombre y la extensión por el parametro ext
+        :param nombre: Nombre del archivo que vamos a crear. type: String
+        :param ext: Nombre de la extensión que tendrá el archivo, puede ser nulo. type: String
+        '''
+        if(ext==''or ext==None or ext==-1):
+                open(str(nombre.get()),'w')
+                tk.messagebox.showinfo(title='Archivo creado',message='El archvo '+str(nombre.get())+' ha sido creado')
+        else:
+                open(str(nombre.get())+'.'+str(ext.current()),'w')
+                tk.messagebox.showinfo(title='Archivo creado',message='El archvo '+str(nombre.get())+'.'+str(ext.current())+' ha sido creado')
+    tk.Button(nueva,text='Aceptar',command=nuevoa).grid(column=0,row=2)
+    nueva.mainloop()
 
 def o():
     pass
@@ -121,8 +126,8 @@ def gui():
     label1.grid(column=0,row=1)
     #
     lista = os.listdir('/mnt'+carp)
-    arch=tk.Listbox(vent_princip)
-    #arch.pack()
+    arch=tk.Listbox(vent_princip,width=40)
+
     for i in lista:
         arch.insert(tk.END, i)
     arch.grid(column=0,row=2) 
