7cecef99a21be5497d6efae05bcb726bd07c3f4c
hectorhmx <hectorhmx.55@gmail.com>
2020-05-26 22:44:14 -0500

    Mejoradas las funciones de fecha

--- proyectos/4/BarreroPatricio-EspinoHector/fecha.py
+++ proyectos/4/BarreroPatricio-EspinoHector/fecha.py
@@ -1,10 +1,20 @@
 import datetime
 
 
-def convertir(fecha_lineal):
+def formatear(fecha_lineal):
     formato = "%Y%m%d%H%M%S"
     return str(datetime.datetime.strptime(fecha_lineal,formato))
 
+def compactar(fecha):
+    formato = "%Y%m%d%H%M%S"
+    formato_original = "%Y-%m-%d %H:%M:%S"
+    fecha_obj=datetime.datetime.strptime(fecha,formato_original)
+    return datetime.datetime.strftime(fecha_obj,formato)
+
+def fecha_actual():
+    return datetime.datetime.now()
+
 if __name__ == "__main__":
-    a = convertir("20190509182600")
+    a = formatear("20190509182600")
     print(a)
+    print(compactar(a))
