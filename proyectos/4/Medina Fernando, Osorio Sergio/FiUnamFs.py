8b97c5d2bb5871ff51dfe90aeb9fea2a7eed0593
fernando170 <fer-170@live.com.mx>
2020-05-26 23:53:55 -0500

    Voy

--- proyectos/4/Medina Fernando, Osorio Sergio/FiUnamFs.py
+++ proyectos/4/Medina Fernando, Osorio Sergio/FiUnamFs.py
@@ -24,64 +24,6 @@ def cierra():
 	fin()
 	pass
 
-
-def nc():
-    nuevo = tk.Toplevel()
-    nuevo.title('Crear nueva carpeta')
-    la=tk.Label(nuevo,text='Por favor ingrese el nombre de la carpeta a crear')
-    la.grid(column=0,row=0)
-    nombrec=tk.Entry(nuevo)
-    nombrec.grid(column=0,row=1)
-    def nuevac():
-        '''
-        Con ésta función crearemos en el disco una carpeta con el nombre específicado en el parámetro nom, en dado caso de que no se pueda generar la carpeta se informará al usuario a través de una ventana.
-        :param nombre: El nombre que se le dará a la nueva carpeta. type: String
-        '''
-        try:
-            os.mkdir('/mnt'+carp+str(nombrec.get()))
-            tk.messagebox.showinfo(title='Carpeta creada',message='La carpeta '+str(nombre.get())+' ha sido creada')
-        except OSError as e:
-            if e.errno == errno.EEXIST:
-                print('Directory not created.')
-            else:
-                raise
-                tk.messagebox.showinfo(title='Carpeta no creada',message='Fallo al intentar crear carpeta nueva, es posible que ya exista una con el mismo nombre.')
-                print(sys.exc_info()[0])
-                nuevo.destroy()
-    
-    boton2=tk.Button(nuevo,text='Aceptar',command=nuevac)
-    boton2.grid(column=0,row=3)
-    nuevo.mainloop()
-    '''
-    Con la función nc se pretende desplegar una pantalla en la cual ingresaremos el nombre de la carpeta que será creada a través de la función nuevac.	
-    '''
-
-def na():
-    nueva = tk.Toplevel()
-    nueva.title('Crear nuevo archivo')
-    ins=tk.Label(nueva,text='Por favor ingrese el nombre del archivo y seleccione la extensión deseada').grid(column=0,row=0)
-    nombre=tk.Entry(nueva)
-    nombre.grid(column=0,row=1)
-    ext=ttk.Combobox(nueva, width=27,text='Elija uno')
-    ext['values']=('com','html','docx','pptx','jpg','png','exe','svg','ico')
-    ext.grid(column=2,row=1)
-    print('creación de archivo')
-    def nuevoa():
-        '''
-        #Con la función nuevoa se pretende crear el archivo con la extensión deseada por el usuario, ya sea que el la ingrese manualmente o utilizando el combobox proporcionado con las opciones más comunes, el nombre estará dado por el parámetro nombre y la extensión por el parametro ext
-        #:param nombre: Nombre del archivo que vamos a crear. type: String
-        #:param ext: Nombre de la extensión que tendrá el archivo, puede ser nulo. type: String
-        '''
-        if(ext==''or ext==None or ext==-1):
-                open('/mnt'+carp+str(nombre.get()),'w')
-                tk.messagebox.showinfo(title='Archivo creado',message='El archvo '+str(nombre.get())+' ha sido creado')
-        else:
-                open('/mnt'+carp+str(nombre.get())+'.'+str(ext.current()),'w')
-                tk.messagebox.showinfo(title='Archivo creado',message='El archvo '+str(nombre.get())+'.'+str(ext.current())+' ha sido creado')
-    tk.Button(nueva,text='Aceptar',command=nuevoa).grid(column=0,row=2)
-    nueva.mainloop()
-
-
 def desfragmentar():
     fs.defrag()
     tk.messagebox.showinfo(title='Desfragmentado exitoso',message='EL volumen ha sido desfragmentado exitosamente')
@@ -210,13 +152,9 @@ def gui():
     vent_princip.config(menu=menu)
     filemenu = tk.Menu(menu)
     nuevo_menu=tk.Menu(filemenu)
-    nuevo_menu.add_command(label='Archivo',command=na)
-    nuevo_menu.add_command(label='Carpeta',command=nc)
     menu.add_cascade(label='Archivo', menu=filemenu)
-    #filemenu.add_cascade(label='Nuevo',menu=nuevo_menu)
-#    filemenu.add_command(label='Abrir',command=o)
-    filemenu.add_command(label='Copiar desde el sistema',command=copyout)
-    filemenu.add_command(label='Copiar hacia el sistema',command=copyin)
+    filemenu.add_command(label='Exportar de el sistema',command=copyout)
+    filemenu.add_command(label='Importar hacia el sistema',command=copyin)
     filemenu.add_command(label='Eliminar',command=rm)
     filemenu.add_command(label='Desfragmentar',command=desfragmentar)
     filemenu.add_command(label='Abrir .img',command=oF)
@@ -237,7 +175,6 @@ def gui():
 
     for i in lista:
         trozos=i.split()
-        #ajustar_print(trozos)
         arch.insert(tk.END,ajustar_print(trozos)) 
     arch.grid(column=0,row=3) 
     vent_princip.mainloop()
