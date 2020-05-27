94bbe940db5fb7010020d12cc8ed474caa5578f6
fernando170 <fer-170@live.com.mx>
2020-05-27 01:31:29 -0500

    Programa listo!!!

--- proyectos/4/Medina Fernando, Osorio Sergio/FiUnamFs.py
+++ proyectos/4/Medina Fernando, Osorio Sergio/FiUnamFs.py
@@ -1,3 +1,18 @@
+'''
+ * @Authors: Fernando Arturo Medina Molina, Sergio Osorio Sánchez
+ * @Version: 1.0
+ * @Copyleft:
+ * 1.-Usarla sin ninguna limitación.
+ * 2.-Libertad de estudio (ver cómo está hecho el trabajo).
+ * 3.-(re)distribuir cuantas copias desee.
+ * 4.-Modificarla de la manera que crea conveniente.
+ * @Description: Gracias a lo visto en las clases de teoría de Sistemas Operativos se pretende crear un 'micro-Sistema de archivos' el cual contenga las operaciones simples de Importar, Exportar, Desfragmentar y Eliminar archivos en un Disco o archivo .img proporcionado por el profesor, en este caso será el archivo fiunamfs.py .
+ * @Referencias:  
+ * °https://github.com/gwolf/sistop-2019-2/blob/master/proyectos/3/FranciscoRodrigo-SanchezBeatriz/fsm.py
+ * °https://github.com/unamfi/sistop-2020-1/blob/master/proyectos/3/RosalesRicardo/main.py
+ * @Contact: fernando170@comunidad.unam.mx
+'''
+
 import os
 import sys
 import errno
@@ -11,25 +26,32 @@ import sys
 fs = FIFS()
 
 def inicio():
+    '''
+    Rutina inicial del programa, al ejecutarse desde una terminal, desplegará la información necesaria para que el programa sea ejecutado de manera correcta, ya que son necesarios los permisos de administrador para realizar las operaciones escenciales.
+    Nota.: IMPORTANTE, si el plrograma no esejecutado como administrador o se le brindan los permisos pertinentes no se asegura la correcta ejecución del programa.
+    '''
     print("********************************************************\n**********************ADVERTENCIA***********************\n********************************************************\nLos comandos que seran utilizados requieren de que se \nejecuten como sudo o su, por ello se necesitará la \nejecución de la forma 'sudo python3 FiUnamFs.py' o brindar de \nmanera manual el permiso, cada que se le solicite")
     
-"""FUNCIONALIDADES """
-def oF():
-    nuevo=tk.Toplevel()
-    nuevo.title("Abrir imagen")
-    file_img = filedialog.askopenfilename()
-    
-    
+#*************************FUNCIONES*****************************
 def cierra():
-	fin()
-	pass
+    '''
+    Cerraremos el programa 
+    '''
+    fin()
+	
 
 def desfragmentar():
+    '''
+   Con la funcionalidad desfragmentar lograremos que el volúmen sea desgragmentado gracias a la función defrag() encontrada en el archivo fifs.py y utilizamos dentro de nuestro programa, invocándolo con el comando Desfragmentar ubicado en el menú principal.
+    '''
     fs.defrag()
     tk.messagebox.showinfo(title='Desfragmentado exitoso',message='EL volumen ha sido desfragmentado exitosamente')
 
 
 def rm():
+    '''
+    Gracias a la función 'rm' invocada en el menú por el comando eliminar, despliegaremos una ventana la cual a través de una entrada de texto podremos especificar los inodos que deseamos perder referencia, o eliminar los archivos que especifiquemos, indicando uno por uno el nombre y extensión del archivo deseado. Una vez realizada la eliminación se despliegará un mensaje informativo confirmando dicha operación.
+    '''
     eliminar = tk.Toplevel()
     eliminar.title('ELiminar archivo')
     ins=tk.Label(eliminar,text='Por favor ingrese el nombre del archivo que desea eliminar (incluyendo su extensión).').grid(column=0,row=0)
@@ -46,12 +68,21 @@ def rm():
     eliminar.mainloop()
 
 def copyin():
+    '''
+   Con la funcionalidad de copyin() podemos realizar la operación de transferencia de un archivo local al disco con el que se está operando, la función desplegará un selector de archivos del sistema local, seleccionar el deseado, har click en abrir y el archivo será insertado en el disco.
+    '''
     nuevo=tk.Toplevel()
     nuevo.title("Copiar archivo en el sistema")
     file_c = filedialog.askopenfilename()
     fs.cpin(str(file_c))
     tk.messagebox.showinfo(title='Archivo copiado',message='El archvo '+file_c+' ha sido copiado en el sistema de archivos')
+    
 def ajustar_print(entrada):
+    '''
+    Con ésta función pretendemos ajustar la cadena de caracteres devuelta por la función ls() la cual nos permite mostrar de una manera más organizada los archivos que son listados en el list box, retornará una cadena de caracteres con un formato específico
+    :param: entrada. Será la cadena de caractres a la que se le dará formato. Type: string.
+    :return: nuevaCadena. Es la cadena que retornamos con un formato específico para ser desplegada en el listbox. Type: String.
+    '''
     nuevaCadena = ''
     if len(entrada[0])<15:
         for i in range(14):
@@ -62,7 +93,6 @@ def ajustar_print(entrada):
                 nuevaCadena=nuevaCadena+espacios
     else:
         nuevaCadena=nuevaCadena+entrada[0]                           
-    #print(nuevaCadena)
     nuevaCadena+='   '
     if len(entrada[1])<10:
         for i in range(6):
@@ -124,6 +154,9 @@ def ajustar_print(entrada):
         nuevaCadena=nuevaCadena+entrada[6]
     return(nuevaCadena)
 def copyout():
+    '''
+    Con coupyout() podremos importar al sistema local los archivos que se deseen y se encuentren dentro del disco. La función desiplegará una ventana en la cual solo ingresará en la casilla de texto, el nombre del archivo deseado (junto con su extensión), al hacer click en el botón de aceptar se abrirá otra ventana en la cual tendrá que seleccionar el nombre de la carpeta en la que será alojado el archivo. Finalmente se mostrará un mensaje informativo confirmando la operación.
+    '''
     nuevo=tk.Toplevel()
     nuevo.title("Copiar archivo hacia el sistema local")
     ins=tk.Label(nuevo,text='Por favor ingrese el nombre del archivo que desea copiar a su sistema local de archivos (incluyendo su extensión).').grid(column=0,row=0)
@@ -139,13 +172,25 @@ def copyout():
 
     tk.Button(nuevo,text='Aceptar',command=elimina).grid(column=0,row=2)
     nuevo.mainloop()
-
-'''Crearemos la interfaz grafica con tkinter'''
-def gui():    
+ 
+def ayuda():
+    '''
+    Con la función ayuda podrá acceder a esta documentación la cual será desplegada a través de su navegador. ésta se encuentra en un archivo .html
+    '''
+    aiura = tk.Toplevel()
+    aiura.title('Documentación')
+    a= open("./Documentacion.txt")
+    aiura.geometry('300x300')
+    T = tk.Text(aiura, height=250, width=300)
+    T.pack()
+    T.insert(1.0, a.read())
+    aiura.mainloop()
+#*************Crearemos la interfaz grafica con tkinter***********
+def gui():
+    '''
+    gui() será la funcionalidad más importante, ya que es la ventana principal conteniente de todas las funcionalidades contenientes en el sistema de archivos, así mismo, contiene la función que despliega ésta documentación.
+    '''    
     vent_princip=tk.Tk()
-#    imgicon = tk.PhotoImage(file=os.path.join('favicon.ico'))
-#    vent_princip.tk.call('wm', 'iconbitmap', vent_princip._w, imgicon)  
-#    vent_princip.iconbitmap(os.path.join('favicon.ico'))
     vent_princip.geometry('544x400')
     vent_princip.title('Sistema de Archivos Fi-UNAM')   
     menu = tk.Menu(vent_princip)
@@ -157,13 +202,12 @@ def gui():
     filemenu.add_command(label='Importar hacia el sistema',command=copyin)
     filemenu.add_command(label='Eliminar',command=rm)
     filemenu.add_command(label='Desfragmentar',command=desfragmentar)
-    filemenu.add_command(label='Abrir .img',command=oF)
     filemenu.add_command(label='Eliminar Archivo',command=rm)
     filemenu.add_separator() 
     filemenu.add_command(label='Salir', command=vent_princip.quit) 
     helpmenu = tk.Menu() 
     menu.add_cascade(label='Ayuda', menu=helpmenu) 
-    helpmenu.add_command(label='Acerca de ')
+    helpmenu.add_command(label='Acerca de ', command=ayuda)
     menu.add_command(label='Actualizar',command=gui)
     #  
     label1=tk.Label(vent_princip,text='Listado de archivos')
@@ -179,7 +223,7 @@ def gui():
     arch.grid(column=0,row=3) 
     vent_princip.mainloop()
 
-
+#*********MAIN********
 if __name__=="__main__":
     inicio()
     gui()
